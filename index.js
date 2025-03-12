a4f0386248ae576a4f2a009bd3016277d1c15d47
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-03-13 02:32:33 +0300

    Created Functons

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const headquarters = 42;
+const lengthOfManhattanBlocks = 264;
+let pickupLocation = 50;
+
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(pickupLocation - headquarters);
+}
+console.log(distanceFromHqInBlocks());
+
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) * lengthOfManhattanBlocks;
+}
+console.log(distanceFromHqInFeet());
+
+function distanceTravelledInFeet(startBlock, destinationBlock) {
+  return Math.abs(destinationBlock - startBlock) * lengthOfManhattanBlocks;
+}
+console.log(distanceTravelledInFeet(38, 34));
+
+function calculatesFarePrice(startBlock, destinationBlock) {
+  let distance = distanceTravelledInFeet(startBlock, destinationBlock);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far"; // ✅ Simplified this condition
+  }
+}
